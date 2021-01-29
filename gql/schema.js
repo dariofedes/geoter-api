079f35e77cefa4f6e7bbf745cfa7e4cf473c8b42
Darío Fernández <dariofedes@protonmail.com>
2021-01-29 20:35:51 +0100

    adding user type to gql schema

--- gql/schema.js
+++ gql/schema.js
@@ -1,6 +1,12 @@
 const { gql } = require('apollo-server')
 
 module.exports = gql`
+    type User {
+        id: ID!
+        email: String!
+        username: String!
+    }
+
     type Query {
         test: String
     }
